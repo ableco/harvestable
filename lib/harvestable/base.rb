6a5f8be9a58bcb403b2b515ed362f828accfc4b3
Mike Potter <mpotter1@gmail.com>
2019-02-17 12:36:11 -0800

    `.all` now retrieves all records (auto-paginates).
    This is how we were using this method in our Core implementation (which was then causing bugs), and is also a closer implementation to what `.all` does in ActiveRecord.

--- lib/harvestable/base.rb
+++ lib/harvestable/base.rb
@@ -13,12 +13,47 @@ module Harvestable
     include Her::Model
     use_api -> { Harvestable.configuration.connection }
 
-    def self.page(page)
-      where.page(page)
+    DEFAULT_PER_PAGE = 100
+    DEFAULT_FIRST_PAGE = 1
+
+    # Scopes
+    scope :page, ->(page) { where(page: page) }
+    scope :per_page, ->(per_page) { where(per_page: per_page) }
+
+    # Iterates through each page. Useful for stitching together an array of all
+    # records, across every page. For example:
+    #
+    #   users = User.all.to_a
+    #
+    # If there are 3 total pages (25 records per page), it would result in
+    # these HTTP requests:
+    #
+    #   GET /v1/users
+    #   GET /v1/users?page=2
+    #   GET /v1/users?page=3
+    #
+    #   users.count
+    #   # => 75
+    #
+    # Returns collection of resource objects.
+    def self.all
+      return to_enum(:all) unless block_given?
+
+      get_page(DEFAULT_FIRST_PAGE) do |result|
+        yield result
+      end
     end
 
-    def self.per_page(per_page)
-      where.per_page(per_page)
+    def self.get_page(page_number, &block)
+      response = per_page(DEFAULT_PER_PAGE).page(page_number)
+
+      response.each do |result|
+        yield result
+      end
+
+      return if response.count < DEFAULT_PER_PAGE
+
+      get_page(page_number + 1, &block)
     end
 
     def save
